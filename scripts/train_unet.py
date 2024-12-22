315c87ec2c2c330c67d9d915a2cdec104a0f2299
Darin Chau <darinchauyf@gmail.com>
2024-12-22 21:01:00 +0800

    bugfixes

--- scripts/train_unet.py
+++ scripts/train_unet.py
@@ -13,7 +13,6 @@ import accelerate
 import torch
 import torch.nn.functional as F
 from accelerate import Accelerator, InitProcessGroupKwargs
-from accelerate.logging import get_logger
 from accelerate.utils import ProjectConfiguration, set_seed
 from huggingface_hub import create_repo, upload_folder
 from packaging import version
@@ -25,7 +24,8 @@ import torch
 from torch import nn
 
 import diffusers
-from diffusers import DDPMScheduler, UNet2DModel, UNet2DConditionModel, StableDiffusionPipeline #type: ignore
+from diffusers import StableDiffusionPipeline
+from diffusers import DDPMScheduler, UNet2DModel, UNet2DConditionModel #type: ignore
 from diffusers.schedulers.scheduling_utils import SchedulerMixin
 from diffusers.optimization import get_scheduler
 from diffusers.training_utils import EMAModel
@@ -40,10 +40,24 @@ import wandb
 
 from .test_vqvae import save_vae_output_to_audio
 
+def ass():
+    import torch
+    from diffusers import StableDiffusionPipeline
+
+    model_id = "CompVis/stable-diffusion-v1-4"
+    device = "cuda"
+
+
+    pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
+    pipe = pipe.to(device)
+
+    prompt = "a photo of an astronaut riding a horse on mars"
+    image = pipe(prompt).images[0]
+
+    image.save("astronaut_rides_horse.png")
+
 # Will error if the minimal version of diffusers is not installed. Remove at your own risks.
 check_min_version("0.27.0.dev0")
-
-logger = get_logger(__name__, log_level="INFO")
 
 # The scaling factor for the VAE latents. This is the value used in the original DDPM paper.
 # https://github.com/huggingface/diffusers/blob/c4b5d2ff6b529ac0f895cedb04fef5b25e89c412/src/diffusers/models/autoencoders/vq_model.py
@@ -90,9 +104,7 @@ class TrainingConfig:
     ema_inv_gamma: float
     ema_power: float
     ema_max_decay: float
-    logger: str
-    logging_dir: str
-    local_rank: int
+    pe_norm_loss_weight: float
     mixed_precision: str
     checkpointing_steps: int
     checkpoints_total_limit: int
@@ -142,10 +154,17 @@ def load_vae(args_path: str, args: TrainingConfig, device):
 
 def load_unet_model(args: TrainingConfig, device):
     # Loads the UNet model and scheduler.
-    model_id = args.model_id
+    model_id = "CompVis/stable-diffusion-v1-4"
+    device = "cuda"
 
-    pipe: StableDiffusionPipeline = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16) # type: ignore
+
+    pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
     pipe = pipe.to(device)
+
+    prompt = "a photo of an astronaut riding a horse on mars"
+    image = pipe(prompt).images[0]
+
+    image.save("astronaut_rides_horse.png")
 
     prompt = args.initial_prompt
 
@@ -232,48 +251,26 @@ def save_model(accelerator: Accelerator,
         "unet": unet.state_dict(),
         "prompt_embeds": prompt_embeds.state_dict(),
     }, path)
-    logger.info(f"Saved UNet model to {path}")
+    print(f"Saved UNet model to {path}")
 
     if args.use_ema:
         ema_model.restore(unet.parameters())
 
-def main(config_path: str, vae_ckpt_path: str,
+def main(config_path: str,
+         vae_ckpt_path: str,
          lookup_table_path: str = "./resources/lookup_table_train.json",
-            val_lookup_table_path: str = "./resources/lookup_table_val.json", **kwargs):
+         val_lookup_table_path: str = "./resources/lookup_table_val.json",
+         **kwargs):
 
     args = parse_args(config_path, vae_ckpt_path, lookup_table_path, val_lookup_table_path, **kwargs)
-
-    logging_dir = os.path.join(args.output_dir, args.logging_dir)
-    accelerator_project_config = ProjectConfiguration(project_dir=args.output_dir, logging_dir=logging_dir)
-
-    kwargs = InitProcessGroupKwargs(timeout=timedelta(seconds=7200))  # a big number for high resolution or big dataset
-    accelerator = Accelerator(
-        gradient_accumulation_steps=args.gradient_accumulation_steps,
-        mixed_precision=args.mixed_precision,
-        log_with=args.logger,
-        project_config=accelerator_project_config,
-        kwargs_handlers=[kwargs],
-    )
+    accelerator = Accelerator(mixed_precision=args.mixed_precision)
 
     if args.acc_seed is not None:
         set_seed(args.acc_seed)
 
-    # Make one log on every process with the configuration for debugging.
-    logging.basicConfig(
-        format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
-        datefmt="%m/%d/%Y %H:%M:%S",
-        level=logging.INFO,
-    )
-    logger.info(accelerator.state, main_process_only=False)
-    if accelerator.is_local_main_process:
-        diffusers.utils.logging.set_verbosity_info()
-    else:
-        diffusers.utils.logging.set_verbosity_error()
-
     # Handle the repository creation
-    if accelerator.is_main_process:
-        if args.output_dir is not None:
-            os.makedirs(args.output_dir, exist_ok=True)
+    if args.output_dir is not None:
+        os.makedirs(args.output_dir, exist_ok=True)
 
     # Load pretrained VAE model
     vae = load_vae(config_path, args, accelerator.device)
@@ -306,11 +303,8 @@ def main(config_path: str, vae_ckpt_path: str,
         if is_xformers_available():
             import xformers
 
-            xformers_version = version.parse(xformers.__version__)
-            if xformers_version == version.parse("0.0.16"):
-                logger.warn(
-                    "xFormers 0.0.16 cannot be used for training in some GPUs. If you observe problems during training, please update xFormers to at least 0.0.17. See https://huggingface.co/docs/diffusers/main/en/optimization/xformers for more details."
-                )
+            if version.parse(xformers.__version__) == version.parse("0.0.16"):
+                print("xFormers 0.0.16 cannot be used for training in some GPUs. If you observe problems during training, please update xFormers to at least 0.0.17. See https://huggingface.co/docs/diffusers/main/en/optimization/xformers for more details.")
             model.enable_xformers_memory_efficient_attention()
         else:
             raise ValueError("xformers is not available. Make sure it is installed correctly")
@@ -324,11 +318,9 @@ def main(config_path: str, vae_ckpt_path: str,
         eps=args.adam_epsilon,
     )
 
-    # In distributed training, the load_dataset function guarantees that only one local process can concurrently
-    # download the dataset.
     dataset = load_train_dataset(args)
 
-    logger.info(f"Dataset size: {len(dataset)}")
+    print(f"Dataset size: {len(dataset)}")
 
     train_dataloader = DataLoader(
         dataset, batch_size=args.train_batch_size, shuffle=True, num_workers=args.dataloader_num_workers
@@ -352,23 +344,20 @@ def main(config_path: str, vae_ckpt_path: str,
     if args.use_ema:
         ema_model.to(accelerator.device)
 
-    # We need to initialize the trackers we use, and also store our configuration.
-    # The trackers initializes automatically on the main process.
-    if accelerator.is_main_process:
-        run = os.path.split(__file__)[-1].split(".")[0]
-        accelerator.init_trackers(run)
+    run = os.path.split(__file__)[-1].split(".")[0]
+    accelerator.init_trackers(run)
 
     total_batch_size = args.train_batch_size * accelerator.num_processes * args.gradient_accumulation_steps
     num_update_steps_per_epoch = math.ceil(len(train_dataloader) / args.gradient_accumulation_steps)
     max_train_steps = args.num_epochs * num_update_steps_per_epoch
 
-    logger.info("***** Running training *****")
-    logger.info(f"  Num examples = {len(dataset)}")
-    logger.info(f"  Num Epochs = {args.num_epochs}")
-    logger.info(f"  Instantaneous batch size per device = {args.train_batch_size}")
-    logger.info(f"  Total train batch size (w. parallel, distributed & accumulation) = {total_batch_size}")
-    logger.info(f"  Gradient Accumulation steps = {args.gradient_accumulation_steps}")
-    logger.info(f"  Total optimization steps = {max_train_steps}")
+    print("***** Running training *****")
+    print(f"  Num examples = {len(dataset)}")
+    print(f"  Num Epochs = {args.num_epochs}")
+    print(f"  Instantaneous batch size per device = {args.train_batch_size}")
+    print(f"  Total train batch size (w. parallel, distributed & accumulation) = {total_batch_size}")
+    print(f"  Gradient Accumulation steps = {args.gradient_accumulation_steps}")
+    print(f"  Total optimization steps = {max_train_steps}")
 
     global_step = 0
     first_epoch = 0
@@ -396,86 +385,96 @@ def main(config_path: str, vae_ckpt_path: str,
 
             first_epoch = global_step // num_update_steps_per_epoch
 
+    wandb.init(
+        # set the wandb project where this run will be logged
+        project="vqvae_training-3",
+        config=vars(args),
+    )
+
     # Train!
     for epoch in range(first_epoch, args.num_epochs):
+        mse_losses = []
+        regularizer_losses = []
+        losses = []
+
         model.train()
-        progress_bar = tqdm(total=num_update_steps_per_epoch, disable=not accelerator.is_local_main_process)
-        progress_bar.set_description(f"Epoch {epoch}")
-        for batch in train_dataloader:
-            clean_images = batch["input"].to(weight_dtype)
-            latents, _ = vae.encode(clean_images)
+
+        optimizer.zero_grad()
+
+        for im in tqdm(train_dataloader, desc=f"Epoch {epoch}"):
+            global_step += 1
+
+            # im is (B, 4, 2, 512, 512) -> take the mean of the 2 channels
+            im = im.to(weight_dtype).mean(dim=2)
+            latents, _ = vae.encode(im)
             latents = latents * VAE_SCALING_FACTOR
 
-            # Sample noise that we'll add to the images
             noise = torch.randn(latents.shape, dtype=weight_dtype, device=latents.device)
-            # Sample a random timestep for each image
             timesteps = torch.randint(
-                0, noise_scheduler.config["num_train_timesteps"], (latents.shape[0],), device=clean_images.device
+                0, noise_scheduler.config["num_train_timesteps"], (latents.shape[0],), device=im.device
             ).long()
 
-            # Add noise to the clean images according to the noise magnitude at each timestep
-            # (this is the forward diffusion process)
             noisy_latents = noise_scheduler.add_noise(latents, noise, timesteps) #type: ignore
 
-            with accelerator.accumulate(model):
-                # Predict the noise residual
-                model_output = model(noisy_latents, timesteps).sample
+            # Forward pass
+            with accelerator.autocast():
+                encoder_hidden_states, regularizer_loss = prompt_embeds()
+                model_output = model(noisy_latents, timesteps, encoder_hidden_states).sample
+                mse = F.mse_loss(model_output.float(), noise.float())
 
-                loss = F.mse_loss(model_output.float(), noise.float())  # this could have different weights!
+            # Checkpoint every args.checkpointing_steps steps
+            if global_step % args.checkpointing_steps == 0:
+                if args.checkpoints_total_limit is not None:
+                    checkpoints = os.listdir(args.output_dir)
+                    checkpoints = [d for d in checkpoints if d.startswith("checkpoint")]
+                    checkpoints = sorted(checkpoints, key=lambda x: int(x.split("-")[1]))
 
-                accelerator.backward(loss)
+                    if len(checkpoints) >= args.checkpoints_total_limit:
+                        num_to_remove = len(checkpoints) - args.checkpoints_total_limit + 1
+                        removing_checkpoints = checkpoints[0:num_to_remove]
 
-                if accelerator.sync_gradients:
-                    accelerator.clip_grad_norm_(model.parameters(), 1.0)
+                        logger.info(
+                            f"{len(checkpoints)} checkpoints already exist, removing {len(removing_checkpoints)} checkpoints"
+                        )
+                        logger.info(f"removing checkpoints: {', '.join(removing_checkpoints)}")
+
+                        for removing_checkpoint in removing_checkpoints:
+                            removing_checkpoint = os.path.join(args.output_dir, removing_checkpoint)
+                            shutil.rmtree(removing_checkpoint)
+
+                save_path = os.path.join(args.output_dir, f"checkpoint-{global_step}")
+                accelerator.save_state(save_path)
+
+            # Compute backward pass
+            mse_losses.append(mse.item())
+            regularizer_losses.append(regularizer_loss.item())
+            loss = mse + args.pe_norm_loss_weight * regularizer_loss
+            accelerator.backward(loss)
+
+            if global_step % args.gradient_accumulation_steps == 0:
                 optimizer.step()
                 lr_scheduler.step()
                 optimizer.zero_grad()
 
-            # Checks if the accelerator has performed an optimization step behind the scenes
-            if accelerator.sync_gradients:
-                if args.use_ema:
-                    ema_model.step(model.parameters())
-                progress_bar.update(1)
-                global_step += 1
+            # Update the EMA model
+            if args.use_ema:
+                ema_model.step(model.parameters())
 
-                if accelerator.is_main_process:
-                    if global_step % args.checkpointing_steps == 0:
-                        # _before_ saving state, check if this save would set us over the `checkpoints_total_limit`
-                        if args.checkpoints_total_limit is not None:
-                            checkpoints = os.listdir(args.output_dir)
-                            checkpoints = [d for d in checkpoints if d.startswith("checkpoint")]
-                            checkpoints = sorted(checkpoints, key=lambda x: int(x.split("-")[1]))
-
-                            # before we save the new checkpoint, we need to have at _most_ `checkpoints_total_limit - 1` checkpoints
-                            if len(checkpoints) >= args.checkpoints_total_limit:
-                                num_to_remove = len(checkpoints) - args.checkpoints_total_limit + 1
-                                removing_checkpoints = checkpoints[0:num_to_remove]
-
-                                logger.info(
-                                    f"{len(checkpoints)} checkpoints already exist, removing {len(removing_checkpoints)} checkpoints"
-                                )
-                                logger.info(f"removing checkpoints: {', '.join(removing_checkpoints)}")
-
-                                for removing_checkpoint in removing_checkpoints:
-                                    removing_checkpoint = os.path.join(args.output_dir, removing_checkpoint)
-                                    shutil.rmtree(removing_checkpoint)
-
-                        save_path = os.path.join(args.output_dir, f"checkpoint-{global_step}")
-                        accelerator.save_state(save_path)
-                        logger.info(f"Saved state to {save_path}")
-
-            logs = {"loss": loss.detach().item(), "lr": lr_scheduler.get_last_lr()[0], "step": global_step}
+            logs = {
+                "mse": mse.detach().item(),
+                "regularizer": regularizer_loss.detach().item(),
+                "loss": loss.detach().item(),
+                "lr": lr_scheduler.get_last_lr()[0],
+            }
             if args.use_ema:
                 logs["ema_decay"] = ema_model.cur_decay_value
-            progress_bar.set_postfix(**logs)
-            accelerator.log(logs, step=global_step)
-        progress_bar.close()
+
+            wandb.log(logs, step=global_step)
 
         accelerator.wait_for_everyone()
 
         # Save the model after the epoch
-        if accelerator.is_main_process:
-            save_model(accelerator, args, model, prompt_embeds, ema_model, f"model-{epoch}.pt")
+        save_model(accelerator, args, model, prompt_embeds, ema_model, f"model-{epoch}.pt")
     accelerator.end_training()
 
 if __name__ == "__main__":
