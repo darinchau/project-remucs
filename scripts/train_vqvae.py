24d32939430a0f311e0673abe8a01af0cafbd636
Darin Chau <darinchauyf@gmail.com>
2025-02-22 21:26:56 +0800

    allow multiple discriminator steps before one generator step

--- scripts/train_vqvae.py
+++ scripts/train_vqvae.py
@@ -130,6 +130,14 @@ def get_loss(
     return output, target, losses
 
 
+def cycle_dl(dl):
+    # Helper function to cycle through a dataloader
+    # This prevents O(n) storage of the dataset in memory compared to itertools.cycle
+    while True:
+        for x in dl:
+            yield x
+
+
 def train(config_path: str, output_dir: str, *, start_from_iter: int = 0,
           dataset_params=None, train_params=None, autoencoder_params=None):
     """Retrains the discriminator. If discriminator is None, a new discriminator is created based on the PatchGAN architecture."""
@@ -156,7 +164,7 @@ def train(config_path: str, output_dir: str, *, start_from_iter: int = 0,
     set_seed(train_config['seed'])
 
     # Create the model and dataset #
-    model = VQVAE(
+    model: VQVAE = VQVAE(
         im_channels_in=dataset_config['im_channels'],
         im_channels_out=1,
         model_config=vae_config
@@ -248,176 +256,187 @@ def train(config_path: str, output_dir: str, *, start_from_iter: int = 0,
         discriminator.load_state_dict(disc_sd)
         step_count = start_from_iter
 
+    max_discr_to_gen_ratio = train_config['max_discr_to_gen_ratio']
+    nbatches = num_epochs * len(data_loader)
+
     wandb.init(
         # set the wandb project where this run will be logged
         project=train_config['run_name'],
         config=config
     )
 
-    for epoch_idx in range(num_epochs):
-        recon_losses = []
-        codebook_losses = []
-        perceptual_losses = []
-        commit_losses = []
-        disc_losses = []
-        gen_losses = []
-        losses = []
+    recon_losses = []
+    codebook_losses = []
+    perceptual_losses = []
+    commit_losses = []
+    disc_losses = []
+    gen_losses = []
+    losses = []
 
-        optimizer_g.zero_grad()
+    progress = tqdm(desc='Training (nsteps)')
+    steps_progress = tqdm(desc='Training (noptims)', total=nbatches)
+    dataloader_iter = cycle_dl(data_loader)
+    while step_count < nbatches:
+        step_count += 1
+        steps_progress.update(1)
+        if len(disc_losses) > 0 and len(gen_losses) > 0 and step_count > disc_step_start:
+            disc_to_gen_ratio = int(min(max(disc_losses[-1] / gen_losses[-1], 0), max_discr_to_gen_ratio))
+        elif step_count > disc_step_start:
+            disc_to_gen_ratio = 1
+        else:
+            disc_to_gen_ratio = 0
+
         optimizer_d.zero_grad()
+        optimizer_g.zero_grad()
 
-        for im in tqdm(data_loader):
-            step_count += 1
+        for i in range(disc_to_gen_ratio):
+            #### Optimize Discriminator ####
+            for param in discriminator.parameters():
+                param.requires_grad = True
 
-            # Save the model
-            if step_count % model_save_steps == 0:
-                model_save_path = os.path.join(output_dir, f"vqvae_{step_count}_{train_config['vqvae_autoencoder_ckpt_name']}")
-                disc_save_path = os.path.join(output_dir, f"discriminator_{step_count}_{train_config['vqvae_autoencoder_ckpt_name']}")
-                torch.save(model.state_dict(), model_save_path)
-                torch.save(discriminator.state_dict(), disc_save_path)
+            im = next(dataloader_iter)
+            progress.update(1)
+            with torch.no_grad():
+                output, target, _ = get_loss(
+                    im,
+                    model,
+                    train_config,
+                    reconstruction_loss,
+                    perceptual_loss,
+                    disc_loss,
+                    None
+                )
 
-            output, target, losses = get_loss(
-                im,
-                model,
-                train_config,
-                reconstruction_loss,
-                perceptual_loss,
-                disc_loss,
-                discriminator if step_count > disc_step_start else None
-            )
-
-            ######### Optimize Generator ##########
-            recon_losses.append(losses['recon_loss'].item())
-            codebook_losses.append(losses['codebook_loss'].item() * train_config['codebook_weight'])
-            perceptual_losses.append(losses['lpips_loss'].item())
-            commit_losses.append(losses['commitment_loss'].item() * train_config['commitment_beta'])
-            gen_losses.append(losses['disc_fake_loss'].item() * train_config['disc_weight'])
-
-            g_loss = (
-                losses['recon_loss'] +
-                losses['codebook_loss'] * train_config['codebook_weight'] +
-                losses['commitment_loss'] * train_config['commitment_beta'] +
-                losses['lpips_loss'] * train_config['perceptual_weight'] +
-                losses['disc_fake_loss'] * train_config['disc_weight']
-            ) / acc_steps
-            accelerator.backward(g_loss)
-            #####################################
-
-            ######### Optimize Discriminator #######
-            if step_count > disc_step_start:
-                # Enable grad for discriminator
-                for param in discriminator.parameters():
-                    param.requires_grad = True
-
-                disc_fake_pred: Tensor = discriminator(output[:, None].detach()).squeeze(1)  # (B, 4)
-                disc_real_pred: Tensor = discriminator(target[:, None]).squeeze(1)  # (B, 4)
-                if train_config['disc_loss'] == 'wasserstein':
-                    disc_loss_ = (disc_real_pred - disc_fake_pred).mean()
-                    lip_dist = ((output.detach() - target) ** 2).sum(2).sum(1) ** 0.5 + 1e-8
-                    lip_est = (disc_real_pred - disc_fake_pred).abs().mean() / lip_dist
-                    lip_loss = ((1. - lip_est) ** 2).mean(0).view(())
-                    disc_loss_ += lip_loss
-                else:
-                    disc_fake_loss = disc_loss(disc_fake_pred, torch.zeros(disc_fake_pred.shape, device=disc_fake_pred.device))
-                    disc_real_loss = disc_loss(disc_real_pred, torch.ones(disc_real_pred.shape, device=disc_real_pred.device))
-                    disc_loss_: Tensor = train_config['disc_weight'] * (disc_fake_loss + disc_real_loss) / 2
-                disc_losses.append(disc_loss_.item())
-                disc_loss_ = disc_loss_ / acc_steps
-                accelerator.backward(disc_loss_)
-                if step_count % acc_steps == 0:
-                    optimizer_d.step()
-                    optimizer_d.zero_grad()
-
-                # Disable grad for discriminator
-                for param in discriminator.parameters():
-                    param.requires_grad = False
-            #####################################
-
+            disc_fake_pred: Tensor = discriminator(output[:, None].detach()).squeeze(1)  # (B, 4)
+            disc_real_pred: Tensor = discriminator(target[:, None]).squeeze(1)  # (B, 4)
+            if train_config['disc_loss'] == 'wasserstein':
+                # Implements WGAN-GP with Lipschitz penalty
+                disc_loss_ = (disc_real_pred - disc_fake_pred).mean()
+                lip_dist = ((output.detach() - target) ** 2).sum(2).sum(1) ** 0.5 + 1e-8
+                lip_est = (disc_real_pred - disc_fake_pred).abs().mean() / lip_dist
+                lip_loss = ((1. - lip_est) ** 2).mean(0).view(())
+                disc_loss_ += lip_loss
+            else:
+                disc_fake_loss = disc_loss(disc_fake_pred, torch.zeros(disc_fake_pred.shape, device=disc_fake_pred.device))
+                disc_real_loss = disc_loss(disc_real_pred, torch.ones(disc_real_pred.shape, device=disc_real_pred.device))
+                disc_loss_: Tensor = train_config['disc_weight'] * (disc_fake_loss + disc_real_loss) / 2
+            disc_losses.append(disc_loss_.item())
+            disc_loss_ = disc_loss_ / acc_steps
+            accelerator.backward(disc_loss_)
             if step_count % acc_steps == 0:
-                optimizer_g.step()
-                optimizer_g.zero_grad()
+                optimizer_d.step()
+                optimizer_d.zero_grad()
 
-            # Log losses
+            # Disable grad for discriminator
+            for param in discriminator.parameters():
+                param.requires_grad = False
+
+        if step_count % model_save_steps == 0:
+            model_save_path = os.path.join(output_dir, f"vqvae_{step_count}_{train_config['vqvae_autoencoder_ckpt_name']}")
+            disc_save_path = os.path.join(output_dir, f"discriminator_{step_count}_{train_config['vqvae_autoencoder_ckpt_name']}")
+            torch.save(model.state_dict(), model_save_path)
+            torch.save(discriminator.state_dict(), disc_save_path)
+
+        im = next(dataloader_iter)
+        progress.update(1)
+
+        output, target, losses = get_loss(
+            im,
+            model,
+            train_config,
+            reconstruction_loss,
+            perceptual_loss,
+            disc_loss,
+            discriminator if step_count > disc_step_start else None
+        )
+
+        ######### Optimize Generator ##########
+        recon_losses.append(losses['recon_loss'].item())
+        codebook_losses.append(losses['codebook_loss'].item() * train_config['codebook_weight'])
+        perceptual_losses.append(losses['lpips_loss'].item())
+        commit_losses.append(losses['commitment_loss'].item() * train_config['commitment_beta'])
+        gen_losses.append(losses['disc_fake_loss'].item() * train_config['disc_weight'])
+
+        g_loss = (
+            losses['recon_loss'] +
+            losses['codebook_loss'] * train_config['codebook_weight'] +
+            losses['commitment_loss'] * train_config['commitment_beta'] +
+            losses['lpips_loss'] * train_config['perceptual_weight'] +
+            losses['disc_fake_loss'] * train_config['disc_weight']
+        ) / acc_steps
+        accelerator.backward(g_loss)
+        #####################################
+
+        if step_count % acc_steps == 0:
+            optimizer_g.step()
+            optimizer_g.zero_grad()
+
+        # Log losses
+        wandb.log({
+            "Reconstruction Loss": recon_losses[-1],
+            "Perceptual Loss": perceptual_losses[-1],
+            "Codebook Loss": codebook_losses[-1],
+            "Generator Loss": gen_losses[-1] if gen_losses else 0,
+            "Discriminator Loss": disc_losses[-1] if disc_losses else 0,
+            "Commitment Loss": commit_losses[-1],
+            "log(Reconstruction Loss)": np.log(recon_losses[-1]),
+            "log(Perceptual Loss)": np.log(perceptual_losses[-1]),
+            "log(Codebook Loss)": np.log(codebook_losses[-1]),
+            "log(Generator Loss)": np.log(gen_losses[-1]) if gen_losses else 0,
+            "log(Discriminator Loss)": np.log(disc_losses[-1]) if disc_losses else 0,
+            "log(Commitment Loss)": np.log(commit_losses[-1]),
+            "Discriminator steps": disc_to_gen_ratio
+        }, step=step_count)
+
+        ########### Perform Validation #############
+        val_count_ = 0
+        if step_count % val_steps == 0:
+            model.eval()
+            with torch.no_grad():
+                val_recon_losses = []
+                val_perceptual_losses = []
+                val_codebook_losses = []
+                val_commit_losses = []
+                for val_im in tqdm(val_data_loader, f"Performing validation (step={step_count})", total=min(val_count, len(val_data_loader))):
+                    val_count_ += 1
+                    if val_count_ > val_count:
+                        break
+                    output, target, val_losses = get_loss(
+                        val_im,
+                        model,
+                        train_config,
+                        reconstruction_loss,
+                        perceptual_loss,
+                        disc_loss,
+                        None
+                    )
+                    val_recon_losses.append(val_losses['recon_loss'].item())
+                    val_perceptual_losses.append(val_losses['lpips_loss'].item())
+                    val_codebook_losses.append(val_losses['codebook_loss'].item())
+                    val_commit_losses.append(val_losses['commitment_loss'].item())
+
             wandb.log({
-                "Reconstruction Loss": recon_losses[-1],
-                "Perceptual Loss": perceptual_losses[-1],
-                "Codebook Loss": codebook_losses[-1],
-                "Generator Loss": gen_losses[-1] if gen_losses else 0,
-                "Discriminator Loss": disc_losses[-1] if disc_losses else 0,
-                "Commitment Loss": commit_losses[-1],
-                "log(Reconstruction Loss)": np.log(recon_losses[-1]),
-                "log(Perceptual Loss)": np.log(perceptual_losses[-1]),
-                "log(Codebook Loss)": np.log(codebook_losses[-1]),
-                "log(Generator Loss)": np.log(gen_losses[-1]) if gen_losses else 0,
-                "log(Discriminator Loss)": np.log(disc_losses[-1]) if disc_losses else 0,
-                "log(Commitment Loss)": np.log(commit_losses[-1])
+                "Val Reconstruction Loss": np.mean(val_recon_losses),
+                "Val Perceptual Loss": np.mean(val_perceptual_losses),
+                "Val Codebook Loss": np.mean(val_codebook_losses),
+                "Val Commitment Loss": np.mean(val_commit_losses),
+                "log(Val Reconstruction Loss)": np.log(np.mean(val_recon_losses)),
+                "log(Val Perceptual Loss)": np.log(np.mean(val_perceptual_losses)),
+                "log(Val Codebook Loss)": np.log(np.mean(val_codebook_losses)),
+                "log(Val Commitment Loss)": np.log(np.mean(val_commit_losses))
             }, step=step_count)
 
-            ########### Perform Validation #############
-            val_count_ = 0
-            if step_count % val_steps == 0:
-                model.eval()
-                with torch.no_grad():
-                    val_recon_losses = []
-                    val_perceptual_losses = []
-                    val_codebook_losses = []
-                    val_commit_losses = []
-                    for val_im in tqdm(val_data_loader, f"Performing validation (step={step_count})", total=min(val_count, len(val_data_loader))):
-                        val_count_ += 1
-                        if val_count_ > val_count:
-                            break
-                        output, target, val_losses = get_loss(
-                            val_im,
-                            model,
-                            train_config,
-                            reconstruction_loss,
-                            perceptual_loss,
-                            disc_loss,
-                            None
-                        )
-                        val_recon_losses.append(val_losses['recon_loss'].item())
-                        val_perceptual_losses.append(val_losses['lpips_loss'].item())
-                        val_codebook_losses.append(val_losses['codebook_loss'].item())
-                        val_commit_losses.append(val_losses['commitment_loss'].item())
-
-                wandb.log({
-                    "Val Reconstruction Loss": np.mean(val_recon_losses),
-                    "Val Perceptual Loss": np.mean(val_perceptual_losses),
-                    "Val Codebook Loss": np.mean(val_codebook_losses),
-                    "Val Commitment Loss": np.mean(val_commit_losses),
-                    "log(Val Reconstruction Loss)": np.log(np.mean(val_recon_losses)),
-                    "log(Val Perceptual Loss)": np.log(np.mean(val_perceptual_losses)),
-                    "log(Val Codebook Loss)": np.log(np.mean(val_codebook_losses)),
-                    "log(Val Commitment Loss)": np.log(np.mean(val_commit_losses))
-                }, step=step_count)
-
-                tqdm.write(f"Validation complete: Reconstruction loss: {np.mean(val_recon_losses)}, Perceptual Loss: {np.mean(val_perceptual_losses)}, Codebook loss: {np.mean(val_codebook_losses)}")
-                model.train()
+            tqdm.write(f"Validation complete: Reconstruction loss: {np.mean(val_recon_losses)}, Perceptual Loss: {np.mean(val_perceptual_losses)}, Codebook loss: {np.mean(val_codebook_losses)}")
+            model.train()
+        ############################################
 
         # End of epoch. Clean up the gradients and losses and save the model
         optimizer_d.step()
         optimizer_d.zero_grad()
         optimizer_g.step()
         optimizer_g.zero_grad()
-        if len(disc_losses) > 0:
-            print(
-                'Finished epoch: {} | Recon Loss : {:.4f} | Perceptual Loss : {:.4f} | '
-                'Codebook : {:.4f} | G Loss : {:.4f} | D Loss {:.4f}'.
-                format(epoch_idx + 1,
-                       np.mean(recon_losses),
-                       np.mean(perceptual_losses),
-                       np.mean(codebook_losses),
-                       np.mean(gen_losses),
-                       np.mean(disc_losses)))
-        else:
-            print('Finished epoch: {} | Recon Loss : {:.4f} | Perceptual Loss : {:.4f} | Codebook : {:.4f}'.
-                  format(epoch_idx + 1,
-                         np.mean(recon_losses),
-                         np.mean(perceptual_losses),
-                         np.mean(codebook_losses)))
-
-        model_save_path = os.path.join(output_dir, f"vqvae_epoch_{epoch_idx}_{step_count}_{train_config['vqvae_autoencoder_ckpt_name']}")
-        disc_save_path = os.path.join(output_dir, f"discriminator_epoch_{epoch_idx}_{step_count}_{train_config['vqvae_autoencoder_ckpt_name']}")
+        model_save_path = os.path.join(output_dir, f"vqvae_epoch_{step_count}_{train_config['vqvae_autoencoder_ckpt_name']}")
+        disc_save_path = os.path.join(output_dir, f"discriminator_epoch_{step_count}_{train_config['vqvae_autoencoder_ckpt_name']}")
         torch.save(model.state_dict(), model_save_path)
         torch.save(discriminator.state_dict(), disc_save_path)
 
