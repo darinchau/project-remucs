1ad22a6360e1b51989043c5c371403e7da1fd9ad
Darin Chau <darinchauyf@gmail.com>
2025-02-23 14:29:48 +0800

    new monitor

--- scripts/monitor.py
+++ scripts/monitor.py
@@ -9,6 +9,7 @@ base_dir = "./resources/models/vqvae"
 config_path = "./resources/config/vqvae.yaml"
 FILE_LIMIT = 10
 
+
 def read_config(config_path: str):
     with open(config_path, 'r') as file:
         try:
@@ -18,39 +19,44 @@ def read_config(config_path: str):
             exit(1)
     return config
 
-def main():
-    try:
-        a = int(sys.argv[1])
-    except Exception as e:
-        raise ValueError("Usage: python -m scripts.monitor [START_ITER]")
 
+def delete_stuff(a: int, config):
+    model = os.path.join(base_dir, f"vqvae_{a}_{config['train_params']['vqvae_autoencoder_ckpt_name']}")
+    discr = os.path.join(base_dir, f"vqvae_{a}_{config['train_params']['vqvae_discriminator_ckpt_name']}")
+
+    if os.path.isfile(model) and os.path.isfile(discr):
+        try:
+            os.remove(model)
+            os.remove(discr)
+        except Exception as e:
+            print(e)
+    elif os.path.isfile(discr):
+        try:
+            os.remove(discr)
+        except Exception as e:
+            print(e)
+    elif os.path.isfile(model):
+        try:
+            os.remove(model)
+        except Exception as e:
+            print(e)
+
+
+def main():
     config = read_config(config_path)
 
     while True:
         time.sleep(10)
         if len(os.listdir(base_dir)) < FILE_LIMIT:
             continue
-        model = os.path.join(base_dir, f"vqvae_{a}_{config['train_params']['vqvae_autoencoder_ckpt_name']}")
-        discr = os.path.join(base_dir, f"vqvae_{a}_{config['train_params']['vqvae_discriminator_ckpt_name']}")
 
-        print("Doing a = ", a)
-        if os.path.isfile(model) and os.path.isfile(discr):
-            try:
-                os.remove(model)
-                os.remove(discr)
-            except Exception as e:
-                print(e)
-        elif os.path.isfile(discr):
-            try:
-                os.remove(discr)
-            except Exception as e:
-                print(e)
-        elif os.path.isfile(model):
-            try:
-                os.remove(model)
-            except Exception as e:
-                print(e)
-        a += config['train_params']['autoencoder_img_save_steps']
+        files = os.listdir(base_dir)
+        indices = [int(file.split('_')[1]) for file in files if file.startswith('vqvae')]
+
+        # Delete the oldest model
+        a = min(indices)
+        delete_stuff(a, config)
+        print(f"Deleted model {a}")
 
 
 if __name__ == "__main__":
